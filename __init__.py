a8c191f15ba76142174273f4c3de21dc7bb14289
Elias Ablan <elias.ablan@kavak.com>
2019-06-19 18:00:34 -0400

    Flask instalado

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    return app
+
+html = """
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Page Title</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+        <!--<link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+        <script src='main.js'></script>-->
+    </head>
+    <body>
+        <h1>Hola mundo</h1>
+    </body>
+    </html>
+"""
